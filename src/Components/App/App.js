f3a3d5cdc376edf60b4068a2b25b423aa03a6f72
acandael <info@anthonycandaele.com>
2019-08-12 10:17:10 +0200

    issue with exercise 34

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
       searchResults: [
         { name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
         { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
-        { name: 'name3', artist: 'artist3', album: 'album3', id: 1 }
+        { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
       ]
     };
   }
