df94fb7a7243c32328393e4eb25ed96979438166
acandael <info@anthonycandaele.com>
2019-08-12 22:12:36 +0200

    some update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -35,7 +35,19 @@ class App extends React.Component {
         }
       ]
     };
+
+    this.addTrack = this.addTrack.bind(this);
   }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
     return (
       <div>
@@ -45,7 +57,10 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
