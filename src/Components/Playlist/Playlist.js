bdfe48996f7b5126ae4d46821b5fed62f560eb9f
acandael <info@anthonycandaele.com>
2019-08-13 19:09:04 +0200

    remove tracks from a playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -10,7 +10,8 @@ class Playlist extends React.Component {
         <TrackList
           tracks={this.props.playlistTracks}
           onAdd={this.props.onAdd}
-          isRemoval={false}
+          onRemove={this.props.onRemove}
+          isRemoval={true}
         />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
